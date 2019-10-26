83495a56a8664b48fc3bdf89db0c039a7950b426
Ricardo <rschiavetti@worth.systems>
2019-10-26 13:28:36 +0200

    First Commit

---
+++ src/main/java/com/schiavetti/project/financialcontroller/FinancialControllerApplication.java
@@ -0,0 +1,13 @@
+package com.schiavetti.project.financialcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/schiavetti/project/financialcontroller/FinancialControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schiavetti.project.financialcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
